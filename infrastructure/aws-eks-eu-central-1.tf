8aec6d3714cc1f7498858418cda86efe04aecba0
Jodok Batlogg <jodok@batlogg.com>
2022-12-27 14:05:01 +0100

    remove ipv6 support

--- infrastructure/aws-eks-eu-central-1.tf
+++ infrastructure/aws-eks-eu-central-1.tf
@@ -7,7 +7,7 @@ data "aws_availability_zones" "eu-central-1" {
   provider = aws.eu-central-1
 }
 locals {
-  vpc_cidr_eu-central-1 = "10.1.0.0/16"
+  vpc_cidr_eu-central-1 = "10.2.0.0/16"
   azs_eu-central-1      = slice(data.aws_availability_zones.eu-central-1.names, 0, 3)
 }
 
@@ -23,17 +23,6 @@ module "eks_eu-central-1" {
   cluster_version                = "1.24"
   cluster_endpoint_public_access = true
 
-  # IPV6
-  cluster_ip_family = "ipv6"
-
-  # We are using the IRSA created below for permissions
-  # However, we have to deploy with the policy attached FIRST (when creating a fresh cluster)
-  # and then turn this off after the cluster/node group is created. Without this initial policy,
-  # the VPC CNI fails to assign IPs and nodes cannot join the cluster
-  # See https://github.com/aws/containers-roadmap/issues/1666 for more context
-  # TODO - remove this policy once AWS releases a managed version similar to AmazonEKS_CNI_Policy (IPv4)
-  create_cni_ipv6_iam_policy = true
-
   cluster_addons = {
     coredns = {
       most_recent = true
@@ -42,15 +31,7 @@ module "eks_eu-central-1" {
       most_recent = true
     }
     vpc-cni = {
-      most_recent              = true
-      service_account_role_arn = module.vpc_cni_irsa_eu-central-1.iam_role_arn
-      configuration_values = jsonencode({
-        env = {
-          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
-          ENABLE_PREFIX_DELEGATION = "true"
-          WARM_PREFIX_TARGET       = "1"
-        }
-      })
+      most_recent = true
     }
   }
 
@@ -63,13 +44,6 @@ module "eks_eu-central-1" {
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
     instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
-
-    # We are using the IRSA created below for permissions
-    # However, we have to deploy with the policy attached FIRST (when creating a fresh cluster)
-    # and then turn this off after the cluster/node group is created. Without this initial policy,
-    # the VPC CNI fails to assign IPs and nodes cannot join the cluster
-    # See https://github.com/aws/containers-roadmap/issues/1666 for more context
-    iam_role_attach_cni_policy = true
   }
 
   eks_managed_node_groups = {
@@ -122,24 +96,5 @@ module "vpc_eks_eu-central-1" {
 
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
-  }
-}
-
-module "vpc_cni_irsa_eu-central-1" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "~> 5.0"
-  providers = {
-    aws = aws.eu-central-1
-  }
-
-  role_name_prefix      = "VPC-CNI-IRSA"
-  attach_vpc_cni_policy = true
-  vpc_cni_enable_ipv6   = true
-
-  oidc_providers = {
-    main = {
-      provider_arn               = module.eks_eu-central-1.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:aws-node"]
-    }
   }
 }

9d521ec1b9d44367a45509ce8a19249d13e92a01
Jodok Batlogg <jodok@batlogg.com>
2022-12-27 23:50:29 +0100

    get sso-admin role_arn programatically

--- infrastructure/aws-eks-eu-central-1.tf
+++ infrastructure/aws-eks-eu-central-1.tf
@@ -9,10 +9,19 @@ data "aws_availability_zones" "eu-central-1" {
 data "aws_caller_identity" "current" {
   provider = aws.eu-central-1
 }
+data "aws_iam_roles" "admin_sso_role" {
+  path_prefix = "/aws-reserved/sso.amazonaws.com/"
+  name_regex  = "AWSReservedSSO_AdministratorAccess_.+"
+}
 
 locals {
   vpc_cidr_eu-central-1 = "10.2.0.0/16"
   azs_eu-central-1      = slice(data.aws_availability_zones.eu-central-1.names, 0, 3)
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_roles#role-arns-with-paths-removed
+  admin_sso_role_arn = [
+    for parts in [for arn in data.aws_iam_roles.admin_sso_role.arns : split("/", arn)] :
+    format("%s/%s", parts[0], element(parts, length(parts) - 1))
+  ][0]
 }
 
 module "eks_eu-central-1" {
@@ -44,39 +53,29 @@ module "eks_eu-central-1" {
   control_plane_subnet_ids = module.vpc_eks_eu-central-1.intra_subnets
 
   manage_aws_auth_configmap = true
-
-  aws_auth_roles = [
-    {
-      rolearn  = "arn:aws:iam::908023327186:role/AWSReservedSSO_AdministratorAccess_32220bb7d5ff07a5"
-      username = "Administrators"
-      groups   = ["system:masters"]
-    },
-  ]
   aws_auth_accounts = [
     data.aws_caller_identity.current.account_id
   ]
+  aws_auth_roles = [
+    {
+      rolearn  = local.admin_sso_role_arn
+      username = "aws-sso-admin"
+      groups   = ["system:masters"]
+    },
+  ]
 
   eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
+    ami_type       = "BOTTLEROCKET_x86_64"
+    platform       = "bottlerocket"
     instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
   }
 
   eks_managed_node_groups = {
-    # Default node group - as provided by AWS EKS
-    default_node_group = {
-      # By default, the module creates a launch template to ensure tags are propagated to instances, etc.,
-      # so we need to disable it to use the default template provided by the AWS EKS managed node group service
-      use_custom_launch_template = false
-    }
-
     # Default node group - as provided by AWS EKS using Bottlerocket
     bottlerocket_default = {
       # By default, the module creates a launch template to ensure tags are propagated to instances, etc.,
       # so we need to disable it to use the default template provided by the AWS EKS managed node group service
       use_custom_launch_template = false
-
-      ami_type = "BOTTLEROCKET_x86_64"
-      platform = "bottlerocket"
     }
   }
 }

7af9b7ae688ff79aea3931fdd80594973a08dfe0
Jodok Batlogg <jodok@batlogg.com>
2022-12-28 00:41:34 +0100

    name zone join

--- infrastructure/join_aws-route53.tf
+++ infrastructure/join_aws-route53.tf
@@ -1,4 +1,4 @@
-resource "aws_route53_zone" "main" {
+resource "aws_route53_zone" "join" {
   # only create when running on production
   count = local.environment == "production" ? 1 : 0
   name  = "pro.joinmastodon.org"

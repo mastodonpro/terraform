20bb468cff72eacf2f64b51825fffce47c56a4f8
Jodok Batlogg <jodok@batlogg.com>
2022-12-28 17:07:09 +0100

    log in like kubernetes

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -75,9 +75,17 @@ provider "github" {
   }
 }
 provider "kubectl" {
+  # uses the same configuration as the kubernetes provider
   alias                  = "aws_eu-central-1"
   host                   = module.eks_eu-central-1.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks_eu-central-1.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks_eu-central-1.cluster_name]
+  }
 }
 provider "kubernetes" {
   alias                  = "aws_eu-central-1"

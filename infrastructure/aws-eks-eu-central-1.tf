ea8d316102b2731e2c241f4c95856ef1c1ad1cc5
Jodok Batlogg <jodok@batlogg.com>
2022-12-29 00:37:54 +0100

    create serviceaccounts for external-dns and lb

--- infrastructure/aws-eks-eu-central-1.tf
+++ infrastructure/aws-eks-eu-central-1.tf
@@ -99,3 +99,35 @@ module "vpc_eks_eu-central-1" {
     "kubernetes.io/role/internal-elb" = 1
   }
 }
+
+# Addons
+module "external_dns_irsa_role_eu-central-1" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.9.2"
+  providers = {
+    aws = aws.eu-central-1
+  }
+  role_name                  = "external-dns"
+  attach_external_dns_policy = true
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks_eu-central-1.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:external-dns"]
+    }
+  }
+}
+module "load_balancer_controller_irsa_role_eu-central-1" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.9.2"
+  providers = {
+    aws = aws.eu-central-1
+  }
+  role_name                              = "load-balancer-controller"
+  attach_load_balancer_controller_policy = true
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks_eu-central-1.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}

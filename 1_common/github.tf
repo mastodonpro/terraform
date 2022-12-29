fe0c759d15d237ffa6459a276b68955604e6a3e0
Jodok Batlogg <jodok@batlogg.com>
2022-12-29 12:18:37 +0100

    revert

--- 1_common/github.tf
+++ 1_common/github.tf
@@ -2,15 +2,4 @@ resource "github_repository" "flux" {
   name       = "fleet-infra"
   visibility = "public"
   auto_init  = true
-  security_and_analysis {
-    advanced_security {
-      status = "enabled"
-    }
-    secret_scanning {
-      status = "enabled"
-    }
-    secret_scanning_push_protection {
-      status = "enabled"
-    }
-  }
 }

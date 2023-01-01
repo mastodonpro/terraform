be240cd6e0b92efbf061099b5eff61de2f2f5960
Jodok Batlogg <jodok@batlogg.com>
2023-01-01 21:16:46 +0100

    use alias

--- 4_apps/main.tf
+++ 4_apps/main.tf
@@ -3,6 +3,7 @@ data "tfe_outputs" "infrastructure" {
   workspace    = "infrastructure-${local.environment}"
 }
 data "aws_kms_secrets" "db_aws_eu-central-1" {
+  provider = aws.eu-central-1
   secret {
     name    = "root"
     payload = data.tfe_outputs.infrastructure.values.rds_encrypted_password.eu-central-1
--- 4_apps/mastodon_rds.tf
+++ 4_apps/mastodon_rds.tf
@@ -4,12 +4,14 @@ resource "postgresql_database" "mastodon" {
   name     = "mastodon"
 }
 resource "postgresql_role" "mastodon" {
+  provider = postgresql.aws_eu-central-1
   name     = "mastodon"
   login    = true
   password = data.aws_kms_secrets.db_aws_eu-central-1.plaintext["mastodon"]
 }
 
 resource "postgresql_grant" "mastodon_schema_grant" {
+  provider    = postgresql.aws_eu-central-1
   database    = postgresql_database.mastodon.name
   role        = postgresql_role.mastodon.name
   schema      = "public"
@@ -18,6 +20,7 @@ resource "postgresql_grant" "mastodon_schema_grant" {
 }
 
 resource "postgresql_grant" "mastodon_table_grant" {
+  provider    = postgresql.aws_eu-central-1
   database    = postgresql_database.mastodon.name
   role        = postgresql_role.mastodon.name
   schema      = "public"

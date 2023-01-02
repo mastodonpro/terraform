fb5490a1c170c22e25ca2b58336edea113216748
Jodok Batlogg <jodok@batlogg.com>
2023-01-02 02:30:10 +0100

    allow kustomize-controller to use kms key

--- 2_infrastructure/aws_kms_eu-central-1.tf
+++ 2_infrastructure/aws_kms_eu-central-1.tf
@@ -12,7 +12,6 @@ resource "aws_kms_alias" "sops_eu-central-1" {
 # https://fluxcd.io/docs/guides/mozilla-sops/
 # This is required by flux to be able to decrypt SOPS secrets
 resource "aws_iam_policy" "kms_sops_eu-central-1" {
-  provider    = aws.eu-central-1
   name        = "AWSKMSSopsIAMPolicy_eu-central-1"
   description = "AWS KMS Policy for SOPS - required by eks/flux"
   policy = jsonencode({
@@ -24,6 +23,24 @@ resource "aws_iam_policy" "kms_sops_eu-central-1" {
           "kms:Decrypt",
           "kms:ReEncrypt*",
           "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ],
+        "Effect" : "Allow",
+        "Resource" : "${aws_kms_key.sops_eu-central-1.arn}"
+      }
+    ]
+  })
+}
+# Kustomize-controller needs access to the KMS key
+resource "aws_iam_role" "kustomize-controller_eu-central-1" {
+  name        = "kustomize-controller_eu-central-1"
+  description = "AWS KMS Policy for SOPS - required by eks/flux"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : [
+          "kms:Decrypt",
           "kms:DescribeKey"
         ],
         "Effect" : "Allow",

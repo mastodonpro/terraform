27c9b76972b193bc2c3640cf865f5f3602ed27d1
Jodok Batlogg <jodok@batlogg.com>
2023-01-03 23:18:43 +0100

    set template parameters

--- 2_infrastructure/aws_elasticache_eu-central-1.tf
+++ 2_infrastructure/aws_elasticache_eu-central-1.tf
@@ -22,24 +22,25 @@ resource "aws_security_group" "redis_eu-central-1" {
 
 # https://github.com/hashicorp/terraform-provider-aws/issues/22123#issuecomment-989652326
 resource "aws_elasticache_replication_group" "redis_eu-central-1" {
-  provider                   = aws.eu-central-1
-  description                = "encrpytion enabled"
-  replication_group_id       = "redis-encrypted"
-  at_rest_encryption_enabled = true
-  transit_encryption_enabled = true
-}
-resource "aws_elasticache_cluster" "redis_eu-central-1" {
   provider             = aws.eu-central-1
-  replication_group_id = aws_elasticache_replication_group.redis_eu-central-1.id
-  cluster_id           = "redis"
-  engine               = "redis"
+  replication_group_id = "redis"
+  description          = "encrpytion enabled"
   node_type            = "cache.t4g.micro"
-  num_cache_nodes      = 1
+  engine               = "redis"
   parameter_group_name = "default.redis7"
   engine_version       = "7.0"
+
+  at_rest_encryption_enabled = true
+  transit_encryption_enabled = true
+
   security_group_ids = [
     aws_default_security_group.eu-central-1.id,
     aws_security_group.redis_eu-central-1.id,
   ]
   subnet_group_name = aws_elasticache_subnet_group.redis_eu-central-1.name
+}
+resource "aws_elasticache_cluster" "redis_eu-central-1" {
+  provider             = aws.eu-central-1
+  replication_group_id = aws_elasticache_replication_group.redis_eu-central-1.id
+  cluster_id           = "redis"
 }
